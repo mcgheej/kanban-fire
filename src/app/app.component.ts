9c6b8e5043a47ef49ddf9dbcab4e527fc8e5941b
mcghee.j@outlook.com <mcghee.j@outlook.com>
2023-12-06 16:06:35 +0000

    Completed step 5 - drag & drop for tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
+import {
+  CdkDragDrop,
+  DragDropModule,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Task } from './task/task';
@@ -8,7 +14,14 @@ import { TaskComponent } from './task/task.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, MatToolbarModule, MatIconModule, TaskComponent],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    TaskComponent,
+    DragDropModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -23,4 +36,24 @@ export class AppComponent {
       description: 'Using Firebase and Angular create a Kanban app!',
     },
   ];
+
+  inProgress: Task[] = [];
+  done: Task[] = [];
+
+  editTask(list: string, task: Task): void {}
+
+  drop(event: CdkDragDrop<Task[]>): void {
+    if (event.previousContainer === event.container) {
+      return;
+    }
+    if (!event.container.data || !event.previousContainer.data) {
+      return;
+    }
+    transferArrayItem(
+      event.previousContainer.data,
+      event.container.data,
+      event.previousIndex,
+      event.currentIndex
+    );
+  }
 }
